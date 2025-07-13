27a2dc840960138f7028a22d8aaf72e78682fedb
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-13 07:12:56 +0530

    Update tuition_app_streamlit.py

--- tuition_app_streamlit.py
+++ tuition_app_streamlit.py
@@ -103,34 +103,9 @@ elif role == "Teacher":
 if st.session_state.user_name:
     st.markdown(f"### Welcome, {st.session_state.user_name}")
     if st.button("Logout"):
-    # Logout button fixed to bottom
-st.markdown(
-    """
-    <style>
-        .logout-button {
-            position: fixed;
-            bottom: 20px;
-            left: 50%;
-            transform: translateX(-50%);
-            z-index: 999;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-logout_placeholder = st.empty()
-with logout_placeholder.container():
-    if st.button("Logout", key="logout", help="Click to logout", use_container_width=True):
-        st.session_state.logged_in = False
-        st.session_state.user_role = None
         st.session_state.user_name = ""
-        st.session_state.email = ""
+        st.session_state.user_role = ""
         st.experimental_rerun()
-    #if st.button("Logout"):
-        #st.session_state.user_name = ""
-        #st.session_state.user_role = ""
-        #st.experimental_rerun()
     if st.session_state.user_role == "teacher":
         st.subheader("Upload Homework")
         cls = st.selectbox("Select Class", [f"{i}th" for i in range(6, 13)])

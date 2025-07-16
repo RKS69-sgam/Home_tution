3429173359694e95275cfc59be6589c94d9770e7
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-16 07:22:13 +0530

    Update generate_word.py

--- generate_word.py
+++ generate_word.py
@@ -1,4 +1,7 @@
-try:
+import streamlit as st
+
+def generate_letter():
+    try:
     sheet = client.open_by_key("1aCnuMxOlsJ3VkleK4wgTvMx2Sp-9pAMH")
     st.success("✅ Sheet Accessed Successfully")
 except Exception as e:

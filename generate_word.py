823f288a7f2e71e74e1be416a94e8e2354260b37
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-16 07:24:03 +0530

    Update generate_word.py

--- generate_word.py
+++ generate_word.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def generate_letter():
     try:
-    sheet = client.open_by_key("1aCnuMxOlsJ3VkleK4wgTvMx2Sp-9pAMH")
-    st.success("✅ Sheet Accessed Successfully")
+       sheet = client.open_by_key("1aCnuMxOlsJ3VkleK4wgTvMx2Sp-9pAMH")
+       st.success("✅ Sheet Accessed Successfully")
 except Exception as e:
     st.error(f"❌ Error: {e}")

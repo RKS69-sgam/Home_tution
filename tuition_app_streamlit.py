43d63f58422547787308a2be4b5172dfa1d5b524
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-22 12:10:15 +0530

    Update tuition_app_streamlit.py

--- tuition_app_streamlit.py
+++ tuition_app_streamlit.py
@@ -297,8 +297,7 @@ if st.session_state.logged_in:
             df_teachers = load_data(TEACHER_SHEET)
             st.dataframe(df_teachers)
 
-    
-elif current_role == "teacher":
+ elif current_role == "teacher":
     st.header(f"🧑‍🏫 Teacher Dashboard: Welcome {st.session_state.user_name}")
     
     # Display a summary of today's submitted homework

09a8ba3788bdcc730772a22c799d667294b0e53d
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-26 10:44:24 +0530

    Update tuition_app_streamlit.py

--- tuition_app_streamlit.py
+++ tuition_app_streamlit.py
@@ -98,9 +98,9 @@ if not st.session_state.logged_in:
     st.markdown(f"""<div style="text-align: center;"><h2>Excellent Public School High-tech Homework System 📈</h2></div>""", unsafe_allow_html=True)
     col1, col2 = st.columns(2)
     with col1:
-        st.image("PRK_logo.jpg", use_column_width=True)
+        st.image("PRK_logo.jpg", use_container_width=True)
     with col2:
-        st.image("Excellent_logo.jpg", use_column_width=True)
+        st.image("Excellent_logo.jpg", use_container_width=True)
     st.markdown("---")
     
     option = st.sidebar.radio("Select an option:", ["Login", "New Registration"])

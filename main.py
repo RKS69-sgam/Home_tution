6476637de20b49d5021e0c20c93862c8343e2f76
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-26 15:14:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,6 @@ if not st.session_state.logged_in:
     st.sidebar.title("Login / New Registration")
 
     st.markdown(f"""<div style="text-align: center;"><h2>EPS High-tech Homework System 📈</h2></div>""", unsafe_allow_html=True)
-    print ("All copyright PRK Online Home Tuition app")
     # Use columns to display logos side-by-side
     col1, col2 = st.columns(2)
     with col1:
@@ -93,6 +92,7 @@ if not st.session_state.logged_in:
     with col2:
         try:
             st.image("Excellent_logo.jpg", use_container_width=True)
+            print ("All copyright PRK Online Home Tuition app")
         except FileNotFoundError:
             st.error("Excellent_logo.jpg not found.")
             
